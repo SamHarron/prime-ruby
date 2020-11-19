557c24a7efb5c08f72db58f3ebef8d7d8f7bf5f3
SamHarron <harronsam@gmail.com>
2020-11-19 13:27:22 -0500

    commit tree 29ea8e7541553a5a7ee3fd177be8d3f931a37b5c

--- prime.rb
+++ prime.rb
@@ -1,1 +1,36 @@
-# Add  code here!
+def prime?(num)
+    if num < 2
+        return false
+    end
+    i = 2
+    while i <= num / 2
+        if num % i == 0
+            return false
+        end
+        i += 1
+    end
+    return true
+end
+
+puts prime?(1)
+puts prime?(2)
+puts prime?(3)
+puts prime?(4)
+puts prime?(5)
+puts prime?(6)
+puts prime?(7)
+puts prime?(8)
+puts prime?(9)
+puts prime?(10)
+puts prime?(-1)
+puts prime?(-2)
+puts prime?(-3)
+puts prime?(-4)
+puts prime?(-5)
+puts prime?(-6)
+puts prime?(-7)
+puts prime?(-8)
+puts prime?(-9)
+puts prime?(-10)
+puts prime?(700)
+puts prime?(11)
